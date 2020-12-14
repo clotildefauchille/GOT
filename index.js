e71bd3cb10ae1011191bd7e30c95dbb9537de56b
clotildefauchille <clotildefauchille@gmail.com>
2020-12-14 09:40:23 +0100

    index ok

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const router = require('./app/router');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+app.use(express.static('./public'));
+
+app.use(router);
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
